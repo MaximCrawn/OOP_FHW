8eab19f57c3f8acd36e92389e755d342983d2e9b
Maxim <Crawn@crawn.ru>
2024-03-19 19:21:27 +0300

    Добавил решение Задачи

---
+++ CalculatorLogger.java
@@ -0,0 +1,7 @@
+package HW_6;
+
+public class CalculatorLogger {
+    public static void log(String message) {
+        System.out.println("Журнал: " + message);
+    }
+}
---
+++ Operation.java
@@ -0,0 +1,5 @@
+package HW_6;
+
+public interface Operation {
+    double execute(double num1, double num2);
+}
